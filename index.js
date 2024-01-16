e23c3ede1aef5f3662d921567bbdd833b11d8468
Freddy Lau <freddylaudesign@gmail.com>
2024-01-16 17:43:53 -0500

    worked on prompt messages

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const fs = require('fs')
+const inquirer = require('inquirer')
+const questions = require('./lib/questions')
+const generateSVG = require('./lib/generateSVG')
+
+inquirer.prompt(questions).then((answerObj) => {
+    const svgFile = generateSVG(answerObj)
+    writeFile('./examples/generated.SVG', svgFile)
+})
