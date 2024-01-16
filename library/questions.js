b792527e0067968ae348123902c0240488ae4a19
Freddy Lau <freddylaudesign@gmail.com>
2024-01-16 17:10:09 -0500

    init

---
+++ library/questions.js
@@ -0,0 +1,18 @@
+const questions = [
+    {
+        type: 'input',
+        name: 'letters',
+        message: 'Please enter three letters'
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Please enter color keyword or value'
+    },
+    {
+        type: 'list',
+        name: 'shapes',
+        message: 'Please choose a shape',
+        choices: ['Circle', 'Triangle', 'Square']
+    }
+]
